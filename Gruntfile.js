1e2fe96617fe6ba7bc7a170cef642391df10f743
Unknown <khaledkzy@gmail.com>
2018-03-02 19:32:15 +0000

    feat: add grunt file to crop images

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'gm',
+          sizes: [{
+            width: 240,
+            suffix: '_small_2x',
+            quality: 20
+          }, {
+            width: 480,
+            suffix: '_medium_1x',
+            quality: 40
+          }, {
+            width: 720,
+            suffix: '_large_2x',
+            quality: 60
+          }]
+        },
+
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'imag/',
+          dest: 'images_responsive/'
+        }]
+      }
+    },
+  });
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+};
